0b3f9ed8f1f9d8efed4a1c8dc06b9e764f985094
Sebas93cay <sebas93cay@gmail.com>
2021-04-01 15:59:53 +0000

    Create executer child (grandson proccess)
    This was to make the child process free the buffer after it's terminated
    
    But bad thing happend: when executer finish his job, valgrind states
    that a memory lost happened. Not sure if this is bad or does not matter

--- headershell.h
+++ headershell.h
@@ -28,17 +28,8 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/*String linked list functions*/
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_n(list_t **head, const char *str, size_t n);
-list_t *add_node_end(list_t **head, const char *str);
-list_t *add_node_n_end(list_t **head, const char *str, size_t n);
-void free_list(list_t *head);
-
-/*Miscelaneous functions*/
-
+/*shell*/
+void create_child(pid_t *child_pid);
 
 /* miscellaneous functions */
 /*miscellaneous_1.c string function*/
@@ -49,14 +40,21 @@ int _strncmp(char *s1, char *s2, unsigned int n);
 /*miscellaneous_2.c*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-
 /*miscellaneous_3.c*/
-
 
 /*miscellaneous_4.c enviroment functions*/
 char *_getenv(const char *var);
 void _printenv(void);
 int _setenv(char *name, char *value, int overwrite);
+
+/*String linked list functions*/
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_n(list_t **head, const char *str, size_t n);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_n_end(list_t **head, const char *str, size_t n);
+void free_list(list_t *head);
 
 
 #endif
--- shell.c
+++ shell.c
@@ -1,5 +1,7 @@
 #include "headershell.h"
 
+
+
 void childError(pid_t *child)
 {
 	_printf("Child %u not created\n", *child);
@@ -11,37 +13,66 @@ int main(__attribute__ ((unused)) int argc,
 {
 	size_t buffSize = 1;
 	char *args[2];
-	pid_t child_pid;
+	pid_t child_pid, executer_pid;
 	int status;
 
+	globalhijos = 0;
 	args[0] = malloc(sizeof(*args[0]) * buffSize);
 	args[1] = NULL;
 	if (!args[0])
 		perror(argv[0]);
 
+	_printf("(%u)I'm the father jojojo\n", getpid());
 	while (1)
 	{
 		if (isatty(0))
 			_printf("#cisfun$ ");
 		if (getline(&args[0], &buffSize, stdin) == EOF)
 		{
+			free(args[0]);
 			write(1, "\n", 1);
 			return (0);
 		}
 		args[0][_strlen(args[0]) - 1] = '\0'; /*Remove new line*/
-		child_pid = fork();
-		if (child_pid == -1)
-			childError(&child_pid);
+		create_child(&child_pid);
+		_printf("(%u)ya creamos nuestro primer hijo\n", getpid());
 		if (child_pid == 0)
 		{
-			if (execve(args[0], args, NULL) == -1)
-				perror(argv[0]);
-			break;
+			create_child(&executer_pid);
+			if (executer_pid == 0)
+			{
+				if (execve(args[0], args, NULL) == -1)
+					perror(argv[0]);
+				break;
+			}
+			else
+			{
+				wait(&status);
+				free(args[0]);
+				_printf("(%u) liberamos args[0]\n", getpid());
+				break;
+			}
+			_printf("(%u) salimos de if\n", getpid());
 		}
 		else
-		{
 			wait(&status);
-		}
+		_printf("(%u) salimos de if\n", getpid());
 	}
 	return (0);
+}
+
+
+void create_child(pid_t *child_pid)
+{
+	*child_pid = fork();
+	if (*child_pid == -1)
+		childError(child_pid);
+	if (*child_pid != 0)
+	{
+		_printf("(%u) My son is alive, his name is %u\n", getpid(), *child_pid);
+	}
+	else
+	{
+		_printf("(%u) Yes! I'm alive! my father is %u\n", getpid(), getppid());
+	}
 }
