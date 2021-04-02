a63bc7b0743945dc74f9f467b72da20d7f47c384
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-04-02 14:39:31 +0000

    Some test to make own getline

--- Seb_at_Work/seb_pruebas.c
+++ Seb_at_Work/seb_pruebas.c
@@ -1,13 +1,25 @@
 #include "../headershell.h"
 
+
 int main(int argc, char **argv, char **env)
 {
-	int a;
+	char buff[1000];
+	FILE *stream;
+	char *searcher = NULL;
 
-	while (1)
-	{
-		scanf("%d", &a);
-		printf("a = %d\n", a);
-	}
+	stream = stdin;
+
+	read(0, buff, 2);
+	write(1, buff, 2);
+	write(1, "-Que cosas-", 11);
+	read(0, buff, 2);
+	write(1, buff, 2);
+
+
+
+	searcher = memchr((void *)(stdin->_IO_read_ptr), 's', 100);
+	printf("searcher = %p\n", searcher);
+	printf("read_ptr = %p\n", (void *)stdin->_IO_read_ptr);
+	printf("read_end = %p\n", stdin->_IO_read_end);
 	return (0);
 }
